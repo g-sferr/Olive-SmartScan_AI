70978b4560fa0f24201a4d24f8cbb7d5d3a15b4a
francescoB1997 <francesco.0297@hotmail.it>
2024-07-16 20:50:18 +0200

    Aggiunta la funzione addCrownBBox()

--- src/data_management/data_preprocessing.py
+++ src/data_management/data_preprocessing.py
@@ -5,7 +5,7 @@ import shutil
 
 
 def renameFile():
-    directory = os.path.abspath('../../datasets/Chioma+Olive/')
+    directory = os.path.abspath('datasets/processed/train_set')
     images_dir = os.path.join(directory, 'images')
     labels_dir = os.path.join(directory, 'labels')
 
@@ -34,34 +34,44 @@ def renameFile():
         os.rename( os.path.join(images_dir, image_files[i]), new_img_name)
         os.rename( os.path.join(labels_dir, label_files[i]), new_lbl_name)
         numeric_name += 1
-        print(f"new_img_name: {new_img_name} | new_lbl_name: {new_lbl_name}")
+        #print(f"new_img_name: {new_img_name} | new_lbl_name: {new_lbl_name}")
 
 def copyOnlyLabelsFromImages():
-    destinationDir = os.path.abspath('../../datasets/Chioma+Olive/')
+    destinationDir = os.path.abspath('datasets/processed/train_set')
     images_dest_dir = os.path.join(destinationDir, 'images')
-    labels_dest_dir = os.path.join(destinationDir, 'labels')
+    labels_dest_dir = os.path.join(destinationDir, 'destLbl')
 
-    sourceDir = os.path.abspath('../../datasets/processed/train_set/')
-    labels_dir = os.path.join(sourceDir, 'labels')
+    sourceDir = os.path.abspath('datasets/processed/train_set/')
+    source_labels_dir = os.path.join(sourceDir, 'labels')
 
     image_files = [f for f in os.listdir(images_dest_dir) if f.endswith('.jpg') or f.endswith('.png')]
 
-    label_files = [f for f in os.listdir(labels_dir) if f.endswith('.txt')]
+    label_files = [f for f in os.listdir(source_labels_dir) if f.endswith('.txt')]
 
     for i in range(len(image_files)):
         img_name, img_extension = os.path.splitext(image_files[i])
         for j in range(len(label_files)):
             lbl_name, lbl_extension = os.path.splitext(label_files[j])
             if lbl_name == img_name:
-                shutil.copy( os.path.join(labels_dir, label_files[j]), os.path.join(labels_dest_dir, label_files[j]))
+                shutil.copy( os.path.join(source_labels_dir, label_files[j]), os.path.join(labels_dest_dir, label_files[j]))
                 print(f"Copying {img_name}")
 
 
+def addCrownBBox():
+    directory = os.path.abspath('datasets/processed/train_set')
+    labels_dir = os.path.join(directory, 'destLbl')
+    label_files = [f for f in os.listdir(labels_dir) if f.endswith('.txt')]
 
+    crownBBox = "1 0.5 0.5 0.1 0.1"
+    for i in range(len(label_files)):
+        with open(os.path.join(labels_dir, label_files[i]), 'a') as file:
+            file.write('\n' + crownBBox)
+            print(f"crownBBox added -> {label_files[i]}")
 
 
 
 if __name__ == '__main__':
     #copyOnlyLabelsFromImages()
     renameFile()
+    #addCrownBBox()
     print("OK")
