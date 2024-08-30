9f90894c269435b7a81974e870eeea74529b32a6
francescoB1997 <francesco.0297@hotmail.it>
2024-08-30 18:01:31 +0200

    Modifiche per il test

--- src/inference/evaluate_counting_on_tree.py
+++ src/inference/evaluate_counting_on_tree.py
@@ -14,7 +14,7 @@ def main():
     oliveConCrownLoader = oliveDatasetLoader._load_data(subFolder1)
 
     # Carica il modello YOLO pre-addestrato
-    model = YOLO(r'C:\Users\Francesco\Desktop\Final_Trained_Models\2_YOLOv8 Small\Best_YOLOv8_S.pt')
+    model = YOLO(r'C:\Users\Francesco\Desktop\Final_Trained_Models\5_YOLOv8 XLarge\Best_YOLOv8_X.pt')
 
     true_olive_counts = []
     predicted_olive_counts = []
